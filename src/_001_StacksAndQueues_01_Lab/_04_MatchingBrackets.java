9461b13eb908bbcc5d3f9512333781322100ccce
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-05-13 23:16:30 +0300

    _001_StacksAndQueues_01_Lab

---
+++ src/_001_StacksAndQueues_01_Lab/_04_MatchingBrackets.java
@@ -0,0 +1,29 @@
+package _001_StacksAndQueues_01_Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _04_MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String expression = scanner.nextLine();
+
+        ArrayDeque <Integer> indexes = new ArrayDeque<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char current = expression.charAt(i);
+
+            if (current == '(') {
+                indexes.push(i);
+
+            } else if (current == ')') {
+                int begin = indexes.pop();
+                int end = i + 1;
+
+                String subExpression = expression.substring(begin, end);
+                System.out.println(subExpression);
+            }
+        }
+    }
+}
