15938f161902777aa5b615b8c9567f06990afb6f
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-05-14 00:06:38 +0300

    _001_StacksAndQueues_01_Lab

---
+++ src/_001_StacksAndQueues_01_Lab/_03_DecimalToBinary_01.java
@@ -0,0 +1,28 @@
+package _001_StacksAndQueues_01_Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _03_DecimalToBinary_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        ArrayDeque<Integer> binaryNumber = new ArrayDeque<>();
+
+        if (number == 0) {
+            System.out.print(0);
+        } else {
+            while (number != 0) {
+                int remainder = number % 2;
+                binaryNumber.push(remainder);
+                number /= 2;
+
+            }
+            while (!binaryNumber.isEmpty()) {
+                System.out.print(binaryNumber.pop());
+            }
+        }
+    }
+}
