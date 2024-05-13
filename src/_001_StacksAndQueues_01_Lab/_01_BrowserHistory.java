a93ef652e9e13672c88e0985dfde3a955eaeef8b
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-05-13 23:02:08 +0300

    _001_StacksAndQueues_01_Lab

---
+++ src/_001_StacksAndQueues_01_Lab/_01_BrowserHistory.java
@@ -0,0 +1,35 @@
+package _001_StacksAndQueues_01_Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _01_BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        ArrayDeque<String> historyStack = new ArrayDeque<>();
+        String input = scanner.nextLine();
+
+        String url = null;
+
+        while (!input.equals("Home")) {
+
+            if (input.equals("back")) {
+               if(historyStack.isEmpty()) {
+                   System.out.println("no previous URLs");
+
+               }else  {
+                   url = historyStack.pop();
+                   System.out.println(url);
+               }
+            }else{
+                if(url != null) {
+                   historyStack.push(url);
+                }
+                url = input;
+                System.out.println(url);
+            }
+            input = scanner.nextLine();
+        }
+    }
+}
