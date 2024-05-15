a0d25f4eb08d43a420534f73e19ad74d7cbfa20e
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-05-15 23:03:33 +0300

    _001_StacksAndQueues_02_Exercise_2

---
+++ src/_001_StacksAndQueues_02_Exercise_2/_08_InfixToPostfix.java
@@ -0,0 +1,53 @@
+package _001_StacksAndQueues_02_Exercise_2;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _08_InfixToPostfix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split("\\s+");
+
+        ArrayDeque<String> operatorsStack = new ArrayDeque<>();
+        ArrayDeque<String> outputQueue = new ArrayDeque<>();
+
+        for (String current : input) {
+
+            if (!"-+/*()".contains(current)) {
+                outputQueue.offer(current);
+
+            } else if (current.equals("(")) {
+                operatorsStack.push(current);
+
+            } else if (current.equals(")") && (!operatorsStack.isEmpty() && !operatorsStack.peek().equals("("))) {
+                outputQueue.offer(operatorsStack.pop());
+                operatorsStack.pop();
+
+            } else {
+
+                if ("-+".contains(current)) {
+
+                    while (!operatorsStack.isEmpty() && !operatorsStack.peek().equals("(")) {
+                        outputQueue.offer(operatorsStack.pop());
+                    }
+
+                } else {
+
+                    while (!operatorsStack.isEmpty() && !"+-(".contains(operatorsStack.peek())) {
+                        outputQueue.offer(operatorsStack.pop());
+                    }
+                }
+                operatorsStack.push(current);
+            }
+        }
+
+        while (!operatorsStack.isEmpty()) {
+            outputQueue.offer(operatorsStack.pop());
+        }
+
+        while (!outputQueue.isEmpty()) {
+            System.out.print(outputQueue.pop() + " ");
+        }
+    }
+}
