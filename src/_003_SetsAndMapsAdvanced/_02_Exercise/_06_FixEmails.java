f5ff714fb116b91f6102fa71b02bb0fbb8a0c2c2
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-05-21 23:07:35 +0300

    Exercise

---
+++ src/_003_SetsAndMapsAdvanced/_02_Exercise/_06_FixEmails.java
@@ -0,0 +1,27 @@
+package _003_SetsAndMapsAdvanced._02_Exercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class _06_FixEmails {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> emails = new LinkedHashMap<>();
+
+        String name = scanner.nextLine();
+
+        while (!name.equals("stop")) {
+
+             String email = scanner.nextLine();
+
+             if (!email.endsWith("us") && !email.endsWith("uk") && !email.endsWith("com")) {
+                 emails.put(name, email);
+             }
+
+            name = scanner.nextLine();
+        }
+        emails.entrySet().forEach(entry -> System.out.printf("%s -> %s%n", entry.getKey(), entry.getValue()));
+    }
+}
