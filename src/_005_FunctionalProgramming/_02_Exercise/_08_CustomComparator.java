ffe7d679452d24507b8b20183acf7b5f122ea744
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-05-28 23:31:14 +0300

    Exercise

---
+++ src/_005_FunctionalProgramming/_02_Exercise/_08_CustomComparator.java
@@ -0,0 +1,29 @@
+package _005_FunctionalProgramming._02_Exercise;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class _08_CustomComparator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).toList();
+
+        Comparator<Integer> comparator = (first, second) -> {
+
+            if (first % 2 != 0 && second % 2 == 0) {
+                return 1;
+            }
+
+            if (first % 2 == 0 && second % 2 != 0) {
+                return -1;
+            }
+
+            return first.compareTo(second);
+        };
+
+        numbers.stream().sorted(comparator).forEach(number -> System.out.print(number + " "));
+    }
+}
