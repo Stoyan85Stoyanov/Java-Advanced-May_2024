c35cd11f66cc0b6bd3129e2be8da35485b0f6eb1
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-06-01 13:50:36 +0300

    Exercise

---
+++ src/_006_DefiningClasses/_02_Exercise/_04_RawData/Cargo.java
@@ -0,0 +1,11 @@
+package _006_DefiningClasses._02_Exercise._04_RawData;
+
+public class Cargo {
+    private int cargoWeight;
+    private String cargoType;
+
+    public Cargo(int cargoWeight, String cargoType) {
+        this.cargoWeight = cargoWeight;
+        this.cargoType = cargoType;
+    }
+}
---
+++ src/_006_DefiningClasses/_02_Exercise/_04_RawData/Engine.java
@@ -0,0 +1,15 @@
+package _006_DefiningClasses._02_Exercise._04_RawData;
+
+public class Engine {
+    private int engineSpeed;
+    private int enginePower;
+
+    public Engine(int engineSpeed, int enginePower) {
+        this.engineSpeed = engineSpeed;
+        this.enginePower = enginePower;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+}
