1d6ee0d0cfa7aee81838b302af2611113e9d6507
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-06-03 22:14:42 +0300

    Lab

---
+++ src/_007_Workshop1CustomDataStructures_Lab/Main.java
@@ -0,0 +1,13 @@
+package _007_Workshop1CustomDataStructures_Lab;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CustomStack stack = new CustomStack();
+
+        stack.push(13);
+        stack.push(42);
+        stack.push(69);
+        stack.push(73);
+    }
+}
