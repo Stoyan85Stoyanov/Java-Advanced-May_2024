13e4f3e1dea73c978f80e2442b26609bfeecebd7
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-06-10 19:36:44 +0300

    Exams

---
+++ src/Exams/_18_JavaAdvancedRegularExam26June2021/_03_GroomingSalon/GroomingSalon.java
@@ -0,0 +1,44 @@
+package Exams._18_JavaAdvancedRegularExam26June2021._03_GroomingSalon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroomingSalon {
+
+    private int capacity;
+    private List<Pet> data;
+
+    public GroomingSalon(int capacity) {
+        this.capacity = capacity;
+        this.data = new ArrayList<>();
+    }
+
+    public void add(Pet pet) {
+        if (this.data.size() < this.capacity) {
+            this.data.add(pet);
+        }
+    }
+
+    public boolean remove(String name) {
+        return data.removeIf(pet -> pet.getName().equals(name));
+    }
+
+    public Pet getPet(String name, String owner) {
+        return this.data.stream().filter(car -> car.getName().equals(name)
+                && car.getOwner().equals(owner)).findFirst().orElse(null);
+    }
+
+    public int getCount() {
+        return this.data.size();
+    }
+
+    public String getStatistics() {
+        StringBuilder text = new StringBuilder();
+        text.append("The grooming salon has the following clients").append(":").append(System.lineSeparator());
+        for (Pet pet : data) {
+            text.append(pet.getName()).append(" ").append(pet.getOwner()).append(System.lineSeparator());
+        }
+
+        return text.toString().trim();
+    }
+}
