bde99ff360aa752d48f82947a2e5d85626cbc4c6
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-06-12 21:35:39 +0300

    Exams

---
+++ src/Exams/_07_JavaAdvancedRegularExam17June2023/_03_AutomotiveRepairShop/RepairShop.java
@@ -0,0 +1,43 @@
+package Exams._07_JavaAdvancedRegularExam17June2023._03_AutomotiveRepairShop;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class RepairShop {
+    private int capacity;
+    private List<Vehicle> vehicles;
+
+    public RepairShop(int capacity) {
+        this.capacity = capacity;
+        this.vehicles = new ArrayList<>();
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        if (this.vehicles.size() < this.capacity) {
+            this.vehicles.add(vehicle);
+        }
+    }
+    public boolean removeVehicle(String vin) {
+        return vehicles.removeIf(vehicles -> vehicles.getVIN().equals(vin));
+    }
+
+    public int getCount() {
+        return this.vehicles.size();
+    }
+
+    public Vehicle getLowestMileage() {
+        return vehicles.stream().min(Comparator.comparing(Vehicle::getMileage)).orElse(null);
+    }
+
+    public String report() {
+        StringBuilder text = new StringBuilder();
+        text.append("Vehicles in the preparatory").append(":").append(System.lineSeparator());
+
+        for (Vehicle vehicle : vehicles) {
+            text.append(vehicle.toString()).append(System.lineSeparator());
+        }
+
+        return text.toString().trim();
+    }
+}
