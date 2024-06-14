0ea6f200da891440d9a1625c05d8417a5080a7e4
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-06-14 23:36:16 +0300

    Exercise

---
+++ src/_009_IteratorsAndComparators/_02_Exercise/_01_ListyIterator/Main.java
@@ -0,0 +1,35 @@
+package _009_IteratorsAndComparators._02_Exercise._01_ListyIterator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> elements = Arrays.stream(scanner.nextLine().split("\\s+")).skip(1).collect(Collectors.toList());
+        ListyIterator listyIterator = new ListyIterator(elements);
+
+        String command = scanner.nextLine();
+
+        while (!command.equals("END")) {
+
+            switch (command) {
+                case "HasNext" -> System.out.println(listyIterator.hasNext());
+                case "Move" -> System.out.println(listyIterator.move());
+                case "Print" -> {
+                    try {
+                        listyIterator.print();
+                    } catch (IllegalStateException ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                }
+            }
+            command = scanner.nextLine();
+        }
+    }
+}
