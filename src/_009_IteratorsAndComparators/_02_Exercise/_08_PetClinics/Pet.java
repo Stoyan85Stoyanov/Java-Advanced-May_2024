807e8656e85f6d636c6479f475504ff496bb4efc
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-06-15 14:13:57 +0300

    Exercise

---
+++ src/_009_IteratorsAndComparators/_02_Exercise/_08_PetClinics/Pet.java
@@ -0,0 +1,19 @@
+package _009_IteratorsAndComparators._02_Exercise._08_PetClinics;
+
+public class Pet {
+
+    private final String name;
+    private final int age;
+    private final String kind;
+
+    public Pet(String name, int age, String kind) {
+        this.name = name;
+        this.age = age;
+        this.kind = kind;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d %s",name,age,kind);
+    }
+}
