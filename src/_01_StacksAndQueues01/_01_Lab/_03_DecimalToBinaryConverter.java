d03ed33c8ed83c33ccd6a02d3c673d1fcc6c97f7
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-08-12 19:47:07 +0300

    Lab

---
+++ src/_01_StacksAndQueues01/_01_Lab/_03_DecimalToBinaryConverter.java
@@ -0,0 +1,36 @@
+package _01_StacksAndQueues01._01_Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _03_DecimalToBinaryConverter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int decimalNumber = Integer.parseInt(scanner.nextLine());
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        if (decimalNumber == 0) {
+            System.out.print(0);
+
+        }else {
+            while (decimalNumber != 0) {
+
+                int leftover = decimalNumber % 2;
+                stack.push(leftover);
+                decimalNumber /= 2;
+            }
+        }
+        int stackSize = stack.size();
+
+        for (int i = 0; i < stackSize; i++) {
+            System.out.print(stack.pop());
+        }
+    }
+}
+
+
+
+
+
