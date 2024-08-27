e76420812f27237a76b2da202d33012b7ddbbc9d
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-08-27 21:47:53 +0300

    Exam

---
+++ src/Exams/_03_JavaAdvancedRetakeExam13December2023/fishingPond/Fish.java
@@ -0,0 +1,42 @@
+package Exams._03_JavaAdvancedRetakeExam13December2023.fishingPond;
+
+public class Fish {
+    private String species;
+    private int age;
+    private String matingSeason;
+
+    public Fish(String species, int age, String matingSeason) {
+        this.species = species;
+        this.age = age;
+        this.matingSeason = matingSeason;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setMatingSeason(String matingSeason) {
+        this.matingSeason = matingSeason;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getMatingSeason() {
+        return matingSeason;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("This %s is %d years old and reproduces through %s.", species, age, matingSeason);
+    }
+}
