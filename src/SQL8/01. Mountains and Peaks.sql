4938a08cfbb243a7457d22c455d1cbda9282c1a3
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-10-09 22:12:39 +0300

    sql

---
+++ src/SQL8/01. Mountains and Peaks.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `mountains` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `peaks` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL,
+`mountain_id` INT NOT NULL
+# CONSTRAINT `fk_peaks_mountains`
+# FOREIGN KEY (`mountain_id`)
+# REFERENCES `mountains`(`id`)
+);
+
+ALTER TABLE `peaks`
+ADD CONSTRAINT `fk_peaks_mountains`
+FOREIGN KEY (`mountain_id`)
+REFERENCES `mountains`(`id`)
