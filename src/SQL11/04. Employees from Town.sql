a736010dac5c5ba360d28c9dc50cf44ba26c1449
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-10-14 19:50:16 +0300

    sql

---
+++ src/SQL11/04. Employees from Town.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE usp_get_employees_from_town (searched_town VARCHAR(70))
+BEGIN
+  SELECT `first_name`, `last_name`
+  FROM `employees` AS e
+  JOIN `addresses` AS a USING (address_id)
+  JOIN `towns` AS t USING (town_id)
+  WHERE t.`name` = `searched_town`
+  ORDER BY `first_name`,`last_name`;
+END $$
+DELIMITER ;
+
+CALL usp_get_employees_from_town('Sofia');
