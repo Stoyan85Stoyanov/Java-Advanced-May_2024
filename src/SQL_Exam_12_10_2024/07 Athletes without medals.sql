86e23fe9b41b15b00bf23801626ca0aa9b8acd3f
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-10-19 19:15:37 +0300

    sql

---
+++ src/SQL_Exam_12_10_2024/07 Athletes without medals.sql
@@ -0,0 +1,11 @@
+SELECT
+     a.`id`,
+     a.`first_name`,
+     a.`last_name`
+FROM `athletes` AS a
+LEFT JOIN `disciplines_athletes_medals` AS dam
+ON dam.`athlete_id` = a.`id`
+LEFT JOIN `medals` AS m
+ON dam.`medal_id` = m.`id`
+WHERE m.`id` IS NULL
+ORDER BY a.`id`;
