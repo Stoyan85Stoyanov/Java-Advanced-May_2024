1dfe31475a00e49be595d6c53fab018a495fb7fb
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-10-21 18:21:19 +0300

    sql

---
+++ src/_01_Database_Basics_MySQL_Retake_Exam_7April2024/02. Insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO `students`(`first_name`, `last_name`, `age`, `phone_number`)
+SELECT
+   REVERSE(LOWER(`first_name`)),
+   REVERSE(LOWER(`last_name`)),
+   `age` + SUBSTRING(`phone_number`, 1, 1),
+   CONCAT('1+', `phone_number`)
+FROM `students`
+WHERE `age` < 20;
