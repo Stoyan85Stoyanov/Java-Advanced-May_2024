95a4d6be4c4eb357d7799dc82971e964d947f7bd
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-10-25 22:47:49 +0300

    sql

---
+++ src/_02_DatabaseBasicsMySQLRegularExam_10February2024/05. Most experienced workers.sql
@@ -0,0 +1,9 @@
+USE `preserves_db`;
+
+SELECT CONCAT(`first_name`, ' ', `last_name`) AS 'full_name',
+    TIMESTAMPDIFF(DAY, `start_date`, '2024-01-01') AS 'days_of_experience'
+FROM `workers`
+WHERE
+    TIMESTAMPDIFF(YEAR, `start_date`, CURDATE()) > 5
+    ORDER BY `days_of_experience` DESC
+    LIMIT 10;
