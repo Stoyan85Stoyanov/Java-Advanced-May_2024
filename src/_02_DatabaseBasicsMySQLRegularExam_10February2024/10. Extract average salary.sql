aedcc3bef10e0c79a2bdc1a5e247c56cb48c2149
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-10-26 17:11:52 +0300

    sql

---
+++ src/_02_DatabaseBasicsMySQLRegularExam_10February2024/10. Extract average salary.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+ CREATE FUNCTION udf_average_salary_by_position_name(`position_name` VARCHAR(40))
+     RETURNS DECIMAL(19, 2)
+     DETERMINISTIC
+ BEGIN
+     DECLARE `average_salary` DECIMAL(19, 2);
+     SET `average_salary` := (
+         SELECT ROUND(AVG(w.`salary`) , 2)
+         FROM `workers` AS w
+		INNER JOIN `positions` p
+        ON w.`position_id` = p.`id`
+		WHERE p.`name` = `position_name`);
+     RETURN `average_salary`;
+ END $$
+ DELIMITER ;
+
+  SELECT p.name, udf_average_salary_by_position_name('Forester') as position_average_salary FROM positions p
+ WHERE p.name = 'Forester'
