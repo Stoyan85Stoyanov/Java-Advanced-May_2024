cf1e274f38f4c87fb45f9cadd570defadb88a637
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-11-21 19:36:33 +0200

    sql

---
+++ src/_14_Database_Basics_MySQL_Exam_9_Feb_2020/06. Young offense players without a contract.sql
@@ -0,0 +1,10 @@
+SELECT p.id, concat_ws(' ', p.first_name, p.last_name) as full_name,
+p.age, p.position, p.hire_date
+FROM players as p
+	JOIN
+   skills_data as sd ON p.skills_data_id = sd.id
+WHERE p.age < 23 and
+sd.strength > 50 and
+p.hire_date is NULL and
+p.position = 'A'
+ORDER BY p.salary, p.age;
