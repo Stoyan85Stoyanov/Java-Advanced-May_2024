cf4c470c618a543981eb041ba72f147c8d5860ad
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-11-23 11:02:08 +0200

    sql

---
+++ src/_14_Database_Basics_MySQL_Exam_9_Feb_2020/08. The fastest player by towns.sql
@@ -0,0 +1,16 @@
+SELECT
+    MAX(sd.speed) AS max_speed, tw.name
+FROM
+    skills_data AS sd
+        RIGHT JOIN
+    players AS p ON sd.id = p.skills_data_id
+        RIGHT JOIN
+    teams AS t ON t.id = p.team_id
+        JOIN
+    stadiums AS s ON s.id = t.stadium_id
+        RIGHT JOIN
+    towns AS tw ON tw.id = s.town_id
+WHERE
+    t.name != 'Devify'
+GROUP BY tw.id
+ORDER BY max_speed DESC , tw.name;
